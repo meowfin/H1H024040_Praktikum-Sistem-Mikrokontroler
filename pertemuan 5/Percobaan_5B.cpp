5f33e078480f307505380d3a0516f201de2db393
Arifin Budi Kusuma <budipipin62@gmail.com>
2026-05-12 21:27:57 +0700

    Create Percobaan_5B.cpp

---
+++ pertemuan 5/Percobaan_5B.cpp
@@ -0,0 +1,50 @@
+#include <Arduino_FreeRTOS.h>
+#include <queue.h>
+
+struct readings {
+  int temp;
+  int h;
+};
+
+QueueHandle_t my_queue;
+
+void setup() {
+  Serial.begin(9600);
+  my_queue = xQueueCreate(1, sizeof(struct readings));
+  
+  xTaskCreate(read_data, "read sensors", 128, NULL, 0, NULL);
+  xTaskCreate(display, "display", 128, NULL, 0, NULL);
+}
+
+void loop() {
+  // Kosong dalam FreeRTOS
+}
+
+/*
+ * Task untuk membaca data sensor (simulasi)
+ */
+void read_data(void *pvParameters) {
+  struct readings x;
+  for (;;) {
+    x.temp = 54;
+    x.h = 30;
+    
+    xQueueSend(my_queue, &x, portMAX_DELAY);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
+/*
+ * Task untuk menampilkan data dari queue ke Serial Monitor
+ */
+void display(void *pvParameters) {
+  struct readings x;
+  for (;;) {
+    if (xQueueReceive(my_queue, &x, portMAX_DELAY) == pdPASS) {
+      Serial.print("temp = ");
+      Serial.println(x.temp);
+      Serial.print("humidity = ");
+      Serial.println(x.h);
+    }
+  }
+}
